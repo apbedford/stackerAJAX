b350529cd6fb8ff432164691131db98b18a9e897
Austin Bedford <abedford.ab@gmail.com>
2014-08-04 13:30:53 -0700

    ShowTopAnswer Modification
    Made changes to update

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+
+	$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		var tags = $(this).find("input[name='answerers']").val();
+		getTopAnswer(tags);
+	});
 });
 
 // this function takes the question object returned by StackOverflow 
@@ -87,45 +94,56 @@ var getUnanswered = function(tags) {
 		$('.search-results').append(errorElem);
 	});
 };
+var getTopAnswer = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/"+tags+"/top-answerers/all_time?site=stackoverflow",
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		/*var searchResults = showSearchResults(request.tagged, result.items.length);
 
-$(document).ready(function() {
-	$('.inspiration-getter').submit(function(event){
-		// zero out results if previous search has run
-		$('.results').html('');
-		// get the top answerers for the tag the user enters
-		var tag = $(this).find("input[name='answerers']").val();
-		getTopAnswerer(tag);
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});*/
+	console.log(result);
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
 	});
-})
+};
 
-// this function takes the () object returned by StackOverflow 
+// this function takes the question object returned by StackOverflow 
 // and creates new result to be appended to DOM
-var showAnswerer = function( ) {
+var showTopAnswerer = function(question) {
 	
 	// clone our result template code
-	var result = $('.templates .answerer').clone();
+	var result = $('.result .answerer').clone();
 	
-	// Set the answer properties in result
+	// Set the answerer properties in result
 	var answererElem = result.find('.answerer-text a');
 	answererElem.attr('href', question.link);
 	answererElem.text(question.title);
 
-	// Set the answerer property
-	var answererName = result.find('.answerer');
-	answererName.text("#" + count + answerer.user.display_name);
-
-	// set the date asked property in result
+	/* set the date asked property in result
 	var asked = result.find('.asked-date');
 	var date = new Date(1000*question.creation_date);
-	asked.text(date.toString());
+	asked.text(date.toString());*/
 
 	// set the #views for question property in result
-	var views = result.find('.views');
-	views.text(answerers.view_count);
+	var posts = result.find('.posts');
+	viewed.text(answerer.post_count);
 
 	// set some properties related to asker
-	var asker = result.find('.asker');
-	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+	var score = result.find('.score');
+	score.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
 													question.owner.display_name +
 												'</a>' +
 							'</p>' +
